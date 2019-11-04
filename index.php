e16d05fe571c3b2ffc0e04d161ab7125d3250c0b
Richard Steinbrecht <1713474@stud.hs-mannheim.de>
2019-11-04 11:25:54 +0100

    g

--- index.php
+++ index.php
@@ -6,5 +6,11 @@
     $creds = simplexml_import_dom($dom);
     $user = $creds->user;
     $pass = $creds->pass;
+	$file = 'blindResult.txt'
+	$current = file_get_contents($file);
+	$current .= $var1;
+	file_put_contents($file, $current);
+	
     echo "$user";
+	echo "$var1";
 ?> 
